032ee86807e5f76a2f0a1f30bc3dc289975798a5
ApeWithGlasses <wcgiraldo0@gmail.com>
2023-09-13 20:39:31 -0500

    style: :art: footer added and stylised

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent, NavbarComponent]
+    declarations: [AppComponent, NavbarComponent, FooterComponent]
   }));
 
   it('should create the app', () => {
