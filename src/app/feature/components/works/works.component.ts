615afeca946e3e72b2fa7b31992373a2477f9fbb
ApeWithGlasses <wcgiraldo0@gmail.com>
2024-07-12 23:20:15 -0500

    FoodMe project added

--- src/app/feature/components/works/works.component.ts
+++ src/app/feature/components/works/works.component.ts
@@ -10,7 +10,7 @@ export class WorksComponent {
   works: Works[] = [
     {
       title: 'ModernArtDotCom',
-      url: 'https://apewithglasses.me/ModernArtDotCom/',
+      url: 'https://apewithglasses.github.io/ModernArtDotCom/',
       desc: 'AI generated modern art.',
       img: 'https://i.imgur.com/jkjyTEG.png',
       imgId: 'modernartdotcom',
@@ -23,6 +23,14 @@ export class WorksComponent {
       img: 'https://i.imgur.com/xYUfAmq.png',
       imgId: 'codetoimage',
       altText: 'CodeToImage Banner',
+    },
+    {
+      title: 'FoodMe',
+      url: 'https://foodme-platzi-three.vercel.app/',
+      desc: 'Improved food delivery app.',
+      img: 'https://i.imgur.com/cl6VaLJ.png',
+      imgId: 'foodme',
+      altText: 'FoodMe Banner',
     },
   ];
 }
